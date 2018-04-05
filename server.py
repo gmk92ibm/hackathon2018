29e00d8190a3a50d35ccbacb5a9a7aff70b88e95
Greg Ketterer <Greg.Ketterer@ibm.com>
2018-04-05 08:08:34 -0700

    image upload

--- server.py
+++ server.py
@@ -1,25 +1,23 @@
-#! /usr/bin/env python
-import os
-from flask import Flask, render_template, request, jsonify
+#!/usr/bin/env python
+
+from flask import Flask, render_template, request
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 
 app = Flask(__name__)
 
-port = int(os.getenv('PORT', 3000))
-host = '0.0.0.0'
+photos = UploadSet('photos', IMAGES)
+app.config['UPLOADED_PHOTOS_DEST'] = 'static'
+configure_uploads(app, photos)
 
-@app.route('/')
-def root():
-	return render_template('index.html')
-
-@app.route('/get', methods=['GET'])
-def get():
-	print request.args['text']
-	return jsonify({'data': request.args['text']})
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
 
 @app.route('/post', methods=['POST'])
 def post():
-	print request.form['text']
-	return jsonify({'data': request.form['text']})
+    image = photos.save(request.files['photo'])
+    data = {'text': 'asdf'}
+    return render_template('image.html', image=image, data=data)
 
 if __name__ == '__main__':
-	app.run(host=host, port=port)
+    app.run(host='0.0.0.0', port=3000)
