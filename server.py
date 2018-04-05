b363477aebbfe5332d60a6f4567878a37ae873b2
Greg Ketterer <Greg.Ketterer@ibm.com>
2018-04-05 08:10:33 -0700

    cfenv port

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
-
+import os
 from flask import Flask, render_template, request
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 
 app = Flask(__name__)
+
+port = int(os.getenv('PORT', 3000))
+host = '0.0.0.0'
 
 photos = UploadSet('photos', IMAGES)
 app.config['UPLOADED_PHOTOS_DEST'] = 'static'
@@ -20,4 +23,4 @@ def post():
     return render_template('image.html', image=image, data=data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host=host, port=port)
